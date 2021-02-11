72e57188401313d549ce3da5cce898a1cf381e7b
Tomasz Szoszorek <tomasz.szoszorek@roche.com>
2021-02-11 15:54:17 +0100

    install from files

--- .circleci/install_R_pkgs.sh
+++ .circleci/install_R_pkgs.sh
@@ -1,15 +1,27 @@
 #!/bin/bash 
 set -eo pipefail
-R --vanilla \<<- 'EOF'
+result_line=""
+
+while read -r line
+do
+    echo "line = $line"
+    if [ -n "$result_line" ]; then
+        result_line="$result_line, "
+    fi
+    result_line="$result_line \"$line\""
+done < "r-pkg-deps.txt"
+
+if [ -n "$result_line" ]; then
+R --vanilla <<- 'EOF'
 options(repos = c(CRAN = "https://cloud.r-project.org"))
 .libPaths()
 install.packages(
-    c("testthat", "yaml")
+    c("$result_line")
     )
 EOF
 
 echo "information about R-packages"
-R --vanilla \<<- 'EOF'
+R --vanilla <<- 'EOF'
 installed.packages() -> d1
 print('testthat')
 print(d1[rownames(d1) == 'testthat', c("Version", "LibPath")])
@@ -19,3 +31,4 @@ EOF
 echo 'verify system installation step'
 wget --version
 curl --version
+fi
--- .circleci/install_system_deps.sh
+++ .circleci/install_system_deps.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 set -eo pipefail
+# system-deps.txt file ends with empty line
 
-apt-get update
-apt-get install --no-install-recommends -y wget curl
-apt-get clean
+result_line=""
+
+while read -r line
+do
+    echo "line = $line"
+    result_line="$result_line $line"
+done < "system-deps.txt"
+if [ -n "$result_line" ]; then
+    apt-get update
+    apt-get install --no-install-recommends -y "$result_line"
+    apt-get clean
+fi
 wget --version
 curl --version
