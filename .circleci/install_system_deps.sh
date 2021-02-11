05fd3fe71a057acf138a3068ccc2f0ab468f7dff
Tomasz Szoszorek <tomasz.szoszorek@roche.com>
2021-02-11 15:59:04 +0100

    more debug

--- .circleci/install_system_deps.sh
+++ .circleci/install_system_deps.sh
@@ -4,15 +4,22 @@ set -eo pipefail
 
 result_line=""
 
+pwd
+ls -al
+
 while read -r line
 do
     echo "line = $line"
     result_line="$result_line $line"
 done < "system-deps.txt"
+
+echo "result_line = $result_line"
+
 if [ -n "$result_line" ]; then
     apt-get update
     apt-get install --no-install-recommends -y "$result_line"
     apt-get clean
+    wget --version
+    curl --version
 fi
-wget --version
-curl --version
+
